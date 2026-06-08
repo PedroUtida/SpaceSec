bf8edc41491ab2bdeed368599d00407b272380d4
Joao Lucas Gomes de Souza <joaolucasgdesouza@gmail.com>
2026-06-08 00:33:41 -0300

    fix: correct database relative paths for gradle execution

--- app/src/main/java/com/spaceshield/DatabaseConfig.java
+++ app/src/main/java/com/spaceshield/DatabaseConfig.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class DatabaseConfig {
     
-    private static final String URL = "jdbc:h2:file:./database/spaceshieldDB;INIT=RUNSCRIPT FROM './database/database.sql'";
+    private static final String URL = "jdbc:h2:file:../database/spaceshieldDB;INIT=RUNSCRIPT FROM '../database/database.sql'";
     
     private static final String USER = "sa"; 
     private static final String PASSWORD = ""; 
